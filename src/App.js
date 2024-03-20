10646665866dc402ae853bb20920077dbfa082b1
gurshDhanoa <gursh.dhanoa@gmail.com>
2024-03-20 13:16:01 -0400

    updating router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Newsletter from "./pages/Newsletter.js";
 import Homepage from "./pages/Homepage.js";
 
